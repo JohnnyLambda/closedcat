9e3472558fac978274f1ac4e3a8b44c2acafb5ea
William Edwin Sharkey <williamsharkey@gmail.com>
2018-09-24 02:56:56 -0400

    Add example function for how a User struct can be created

--- user.go
+++ user.go
@@ -7,3 +7,7 @@ type User struct {
 	Email         string
 	EmailVerified bool
 }
+
+func MakeUser(Join time.Time, Email string, EmailVerified bool) User {
+	return User{Join: Join, Email: Email, EmailVerified: EmailVerified}
+}
